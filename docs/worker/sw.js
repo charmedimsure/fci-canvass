941cb40388e355b16a6755937c31399ba54bab61
charmedimsure <als.aldridge@gmail.com>
2026-06-11 11:05:51 -0400

    Fix voter profile: age from DOB, vote count from vns array

--- docs/worker/sw.js
+++ docs/worker/sw.js
@@ -1,6 +1,6 @@
 // FCI FieldMap — Service Worker
 // Cache version: bump this string any time index.html is updated
-const CACHE_NAME = 'fci-fieldmap-v37';
+const CACHE_NAME = 'fci-fieldmap-v38';
 
 const PRECACHE = [
   './',
